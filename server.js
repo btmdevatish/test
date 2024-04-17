ed2ca71cb52575eff0e1d6edb8c79b653245f161
btmdevatish <120098142+btmdevatish@users.noreply.github.com>
2024-04-17 11:41:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const port = 3000;
- 
+ const BuiltTime = require('./built-time');
+module.exports = (request, response) => {
+  response.setHeader('content-type', 'text/plain');
+  response.send(`
+    This Serverless Function was built at ${new Date(BuiltTime)}.
+    The current time is ${new Date()}
+  `);
+};
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
